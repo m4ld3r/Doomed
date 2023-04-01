236a3b922c087b7c0bd2eb7077021743cb41fd97
Nikita Baturin <Banzai.Malder@protonmail.ch>
2023-04-01 20:35:39 +0300

    update README & test zenity with files

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm *~ & rm .*~
-rm NORMA/*~ & rm NORMA/.*~
-rm files/cpp/*~ & rm files/cpp/.*~
-rm files/Pascal/*~ & rm files/Pascal/.*~
-read str
-git add .
-git commit -m "$str"
-git push
