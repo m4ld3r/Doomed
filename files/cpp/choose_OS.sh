aacd5916b5d7599ef5e38335a884c622b08d9b92
Nikita Baturin <Banzai.Malder@protonmail.ch>
2023-05-11 18:07:25 +0300

    в простановочный скрипт добавлены новые популярные дистрибутивы (пока без теста)

--- files/cpp/choose_OS.sh
+++ files/cpp/choose_OS.sh
@@ -2,9 +2,11 @@
 echo -e "\033[31mПроверка вашего Интернет подключения\033[0m"
 wget -q --spider http://google.com
 if [ $? -eq 0 ]; then
-    echo "1) Debian(Ubuntu, Mint, ZorinOS)"
+    echo "1) Debian(Ubuntu, Mint, ZorinOS, ElementaryOS)"
     echo "2) Arch (Manjaro)"
     echo "3) Fedora"
+    echo "4) Slackware"
+    echo "5) Gentoo"
     echo -n "Введите номер вашей ОС:  "
     read choose_OS
     case $choose_OS in
@@ -16,6 +18,12 @@ if [ $? -eq 0 ]; then
             ;;
         3)
             sudo dnf -y update && sudo dnf -y install zenity gcc
+            ;;
+        4)
+            sudo installpkg zenity && sudo installpkg gcc
+            ;;
+        5)
+            sudo emerge sync && sudo emerge zenity && sudo emerge gcc
             ;;
     esac
     ./start
